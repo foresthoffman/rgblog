1abc5df40e105e3bb3d8345c3b970853ffc817fd
Forest Hoffman <foresthoffman@users.noreply.github.com>
2017-08-09 14:01:53 -0700

    Add other foreground color functions

--- rgblog.go
+++ rgblog.go
@@ -15,9 +15,9 @@ import (
 // it in. The output is wrapped with ANSI color escape codes, which can be used to make standard
 // output more readable, in supporting shells.
 //
-// Output color: yellow
-func YPrintf(f string, a ...interface{}) {
-	fmt.Printf("\033[0;33m"+f+"\033[0m", a...)
+// Output color: black
+func BkPrintf(f string, a ...interface{}) {
+	fmt.Printf("\033[0;30m"+f+"\033[0m", a...)
 }
 
 // Formats the output according to the format string and uses the provided interface data to fill
@@ -33,6 +33,42 @@ func RPrintf(f string, a ...interface{}) {
 // it in. The output is wrapped with ANSI color escape codes, which can be used to make standard
 // output more readable, in supporting shells.
 //
+// Output color: green
+func GPrintf(f string, a ...interface{}) {
+	fmt.Printf("\033[0;32m"+f+"\033[0m", a...)
+}
+
+// Formats the output according to the format string and uses the provided interface data to fill
+// it in. The output is wrapped with ANSI color escape codes, which can be used to make standard
+// output more readable, in supporting shells.
+//
+// Output color: yellow
+func YPrintf(f string, a ...interface{}) {
+	fmt.Printf("\033[0;33m"+f+"\033[0m", a...)
+}
+
+// Formats the output according to the format string and uses the provided interface data to fill
+// it in. The output is wrapped with ANSI color escape codes, which can be used to make standard
+// output more readable, in supporting shells.
+//
+// Output color: blue
+func BlPrintf(f string, a ...interface{}) {
+	fmt.Printf("\033[0;34m"+f+"\033[0m", a...)
+}
+
+// Formats the output according to the format string and uses the provided interface data to fill
+// it in. The output is wrapped with ANSI color escape codes, which can be used to make standard
+// output more readable, in supporting shells.
+//
+// Output color: magenta
+func MPrintf(f string, a ...interface{}) {
+	fmt.Printf("\033[0;35m"+f+"\033[0m", a...)
+}
+
+// Formats the output according to the format string and uses the provided interface data to fill
+// it in. The output is wrapped with ANSI color escape codes, which can be used to make standard
+// output more readable, in supporting shells.
+//
 // Output color: cyan
 func CPrintf(f string, a ...interface{}) {
 	fmt.Printf("\033[0;36m"+f+"\033[0m", a...)
@@ -42,7 +78,7 @@ func CPrintf(f string, a ...interface{}) {
 // it in. The output is wrapped with ANSI color escape codes, which can be used to make standard
 // output more readable, in supporting shells.
 //
-// Output color: green
-func GPrintf(f string, a ...interface{}) {
-	fmt.Printf("\033[0;32m"+f+"\033[0m", a...)
+// Output color: white
+func WPrintf(f string, a ...interface{}) {
+	fmt.Printf("\033[0;37m"+f+"\033[0m", a...)
 }
